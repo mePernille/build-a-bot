4c94a8d8744514498a9b5b3ea5ddc70ea44895e7
mePernille <89570696+mePernille@users.noreply.github.com>
2023-12-29 14:38:38 +0100

    Made a new component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />
+        </li>
+      </ul>
+    </nav>
+  </header>
   <main>
-    <HomePage msg="Welcome to Your Vue.js App"/>
+    <!--<HomePage msg="Welcome to Your Vue.js App"/> -->
+    <RobotBuilder/>
 </main>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HomePage,
+    // HomePage,
+    RobotBuilder,
   },
 };
 </script>
@@ -20,16 +32,40 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 
 }
-body{
+
+body {
   background: linear-gradient(to bottom, #555, #999);
   background-attachment: fixed;
 }
 
-main{
+main {
   margin: 0 auto;
   padding: 30px;
   background-color: white;
   width: 1400;
   min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1460px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
